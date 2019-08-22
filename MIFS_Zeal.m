e5ad4e1216392c858dc2b21efb27795a18065461
zealshen <zealshen@outlook.com>
2019-08-22 12:32:22 +0100

    add codes

---
+++ MIFS_Zeal.m
@@ -0,0 +1,13 @@
+function [Rank, Score] = MIFS_Zeal(X, y)
+    
+    num_F = size(X, 2);
+    rank = [];
+    for i = 1:num_F
+%         F_score = -Mutual_Information(X(:, i), y);
+        F_score = -muteinf(X(:, i), y);
+        rank = [rank; F_score i];
+    end
+    rank = sortrows(rank, 1);  
+    Score = rank(1:num_F, 1);
+    Rank = rank(1:num_F, 2);
+end
---
+++ Mutual_Information.m
@@ -0,0 +1,5 @@
+function info = Mutual_Information(X, Y)
+
+    n = size(X, 1);
+
+end
